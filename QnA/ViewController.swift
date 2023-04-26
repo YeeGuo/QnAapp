1ec0cbc229983cbd8e500c1c7d04f99a42911744
YeeGuo <kiwi881217@gmail.com>
2023-04-26 15:31:18 +0800

    Initial Commit

---
+++ QnA/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QnA
+//
+//  Created by 張凱棋 on 2023/4/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
